452af7dc9ab43127d15cf8a23b1563836a4e7f60
Sanchit Verma <sanchit.verma.167@gmail.com>
2019-07-26 22:06:50 +0530

    Circle Alertbox was added to the files

--- app/src/main/java/circlealertbox/circlealertbox/MainActivity.java
+++ app/src/main/java/circlealertbox/circlealertbox/MainActivity.java
@@ -1,13 +1,36 @@
 package circlealertbox.circlealertbox;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
+    Dialog custom_dialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        custom_dialog=new Dialog(this);
+        custom_dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        custom_dialog.setContentView(R.layout.dialog_custom);
+        custom_dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        Button exit_alert = custom_dialog.findViewById(R.id.exit_alert);
+        exit_alert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                custom_dialog.dismiss();
+            }
+        });
+    }
+    public void onButtonClickShowCircleBox(View v)
+    {
+
+        custom_dialog.show();
     }
 }
